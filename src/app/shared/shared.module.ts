d75ed6ec69b0f3098cbf6d3f42c44aadceee550c
Laishyde <laiscristina.animamundi@gmail.com>
2022-11-15 20:51:00 -0300

    feat inclusão de painel

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
-
-
+import { SlideComponent } from './componentes/slide/slide.component';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { PainelExpansionComponent } from './componentes/painel-expansion/painel-expansion.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SlideComponent,
+    PainelExpansionComponent,
+  
+  ],
   imports: [
     CommonModule,
     MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    
   ],
   exports:[
     MatButtonModule,
+    SlideComponent,
+    MatExpansionModule,
+    PainelExpansionComponent,
+    MatIconModule,
+
   ]
 })
 export class SharedModule { }
